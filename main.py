2fcc8521cbb46c432453729535e7dbf8fed63594
reejit <70316178+reejit@users.noreply.github.com>
2022-04-09 07:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 from pyrogram import Client, filters
-from imgbbpy.aio import Client as cl
+import requests
 from dotenv import load_dotenv
 from pyrogram.types import Message
+import base64
+import json
 
 download = "./"
 load_dotenv()
@@ -29,11 +31,18 @@ Send me a picture to get started.
 
 @Bot.on_message(filters.photo & filters.private)
 async def upload_(client: Client, message: Message):
-  imgbb = cl(os.environ.get('IMGBB'))
+  
   file = await bot.download_media(message, DOWNLOAD)
   try:
-    image = await imgbb.upload(file)
-    await message.reply_text(f"Your photo was successful uploaded!\nThe url of the image is {image.url}.")
+    with open(file, "rb") as file:
+    url = "https://api.imgbb.com/1/upload"
+    payload = {
+        "key": os.environ.get('IMGBB'),
+        "image": base64.b64encode(file.read()),
+    }
+    res = requests.post(url, payload).json
+    url = res['data']['url']
+    await message.reply_text(f"Your photo was successful uploaded!\nThe url of the image is {url}.")
     await imgbb.close()
   except Exception as e:
     await message.reply_text("Error:\n{}".format(e))
